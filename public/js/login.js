1f97cfaa40a9785ff593bc4d44bacb00b1c9c25b
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-05-29 12:25:35 -0600

    added javascript code for the login and sign up

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,27 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // This will collect data for the email and password login 
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
 
+  document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
